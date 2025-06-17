eba6740c7eec80dc42a8c7e0160cb4d64617318d
Bingsa <lincolnharrison8pc@gmail.com>
2025-06-17 19:32:23 +0100

    Add files via upload

---
+++ main.bat
@@ -0,0 +1,207 @@
+@echo off
+chcp 65001 >nul
+mode 65,7
+echo ██╗      ██████╗  █████╗ ██████╗ ██╗███╗   ██╗ ██████╗    
+echo ██║     ██╔═══██╗██╔══██╗██╔══██╗██║████╗  ██║██╔════╝    
+echo ██║     ██║   ██║███████║██║  ██║██║██╔██╗ ██║██║  ███╗   
+echo ██║     ██║   ██║██╔══██║██║  ██║██║██║╚██╗██║██║   ██║   
+echo ███████╗╚██████╔╝██║  ██║██████╔╝██║██║ ╚████║╚██████╔╝██╗
+echo ╚══════╝ ╚═════╝ ╚═╝  ╚═╝╚═════╝ ╚═╝╚═╝  ╚═══╝ ╚═════╝ ╚═╝
+title Loading.
+timeout /t 1 /nobreak >nul
+cls
+echo ██╗      ██████╗  █████╗ ██████╗ ██╗███╗   ██╗ ██████╗       
+echo ██║     ██╔═══██╗██╔══██╗██╔══██╗██║████╗  ██║██╔════╝       
+echo ██║     ██║   ██║███████║██║  ██║██║██╔██╗ ██║██║  ███╗      
+echo ██║     ██║   ██║██╔══██║██║  ██║██║██║╚██╗██║██║   ██║      
+echo ███████╗╚██████╔╝██║  ██║██████╔╝██║██║ ╚████║╚██████╔╝██╗██╗
+echo ╚══════╝ ╚═════╝ ╚═╝  ╚═╝╚═════╝ ╚═╝╚═╝  ╚═══╝ ╚═════╝ ╚═╝╚═╝
+title Loading..
+timeout /t 1 /nobreak >nul
+cls                                                      
+echo ██╗      ██████╗  █████╗ ██████╗ ██╗███╗   ██╗ ██████╗          
+echo ██║     ██╔═══██╗██╔══██╗██╔══██╗██║████╗  ██║██╔════╝          
+echo ██║     ██║   ██║███████║██║  ██║██║██╔██╗ ██║██║  ███╗         
+echo ██║     ██║   ██║██╔══██║██║  ██║██║██║╚██╗██║██║   ██║         
+echo ███████╗╚██████╔╝██║  ██║██████╔╝██║██║ ╚████║╚██████╔╝██╗██╗██╗
+echo ╚══════╝ ╚═════╝ ╚═╝  ╚═╝╚═════╝ ╚═╝╚═╝  ╚═══╝ ╚═════╝ ╚═╝╚═╝╚═╝
+title Loading...
+timeout /t 1 /nobreak >nul
+cls
+echo ██╗      ██████╗  █████╗ ██████╗ ██╗███╗   ██╗ ██████╗    
+echo ██║     ██╔═══██╗██╔══██╗██╔══██╗██║████╗  ██║██╔════╝    
+echo ██║     ██║   ██║███████║██║  ██║██║██╔██╗ ██║██║  ███╗   
+echo ██║     ██║   ██║██╔══██║██║  ██║██║██║╚██╗██║██║   ██║   
+echo ███████╗╚██████╔╝██║  ██║██████╔╝██║██║ ╚████║╚██████╔╝██╗
+echo ╚══════╝ ╚═════╝ ╚═╝  ╚═╝╚═════╝ ╚═╝╚═╝  ╚═══╝ ╚═════╝ ╚═╝
+title Loading.
+timeout /t 1 /nobreak >nul
+cls
+echo ██╗      ██████╗  █████╗ ██████╗ ██╗███╗   ██╗ ██████╗       
+echo ██║     ██╔═══██╗██╔══██╗██╔══██╗██║████╗  ██║██╔════╝       
+echo ██║     ██║   ██║███████║██║  ██║██║██╔██╗ ██║██║  ███╗      
+echo ██║     ██║   ██║██╔══██║██║  ██║██║██║╚██╗██║██║   ██║      
+echo ███████╗╚██████╔╝██║  ██║██████╔╝██║██║ ╚████║╚██████╔╝██╗██╗
+echo ╚══════╝ ╚═════╝ ╚═╝  ╚═╝╚═════╝ ╚═╝╚═╝  ╚═══╝ ╚═════╝ ╚═╝╚═╝
+title Loading..
+timeout /t 1 /nobreak >nul
+cls                                                    
+echo ██╗      ██████╗  █████╗ ██████╗ ██╗███╗   ██╗ ██████╗          
+echo ██║     ██╔═══██╗██╔══██╗██╔══██╗██║████╗  ██║██╔════╝          
+echo ██║     ██║   ██║███████║██║  ██║██║██╔██╗ ██║██║  ███╗         
+echo ██║     ██║   ██║██╔══██║██║  ██║██║██║╚██╗██║██║   ██║         
+echo ███████╗╚██████╔╝██║  ██║██████╔╝██║██║ ╚████║╚██████╔╝██╗██╗██╗
+echo ╚══════╝ ╚═════╝ ╚═╝  ╚═╝╚═════╝ ╚═╝╚═╝  ╚═══╝ ╚═════╝ ╚═╝╚═╝╚═╝
+title Loading...
+timeout /t 1 /nobreak >nul
+cls
+
+title AuroraOS ┃ Made by Bingsa
+mode 120,33
+goto mainmenu
+
+:mainmenu
+cls
+echo.                                                          
+echo                                     ██                                                  
+echo                                    ▄██▄                                                 
+echo                                   ▄█▀██▄   ▀███  ▀███ ▀███▄███  ▄██▀██▄▀███▄███ ▄█▀██▄  
+echo                                  ▄█  ▀██     ██    ██   ██▀ ▀▀ ██▀   ▀██ ██▀ ▀▀██   ██  
+echo                                 ████████    ██    ██   ██     ██     ██ ██     ▄█████  
+echo                                █▀      ██   ██    ██   ██     ██▄   ▄██ ██    ██   ██  
+echo                              ▄███▄   ▄████▄ ▀████▀███▄████▄    ▀█████▀▄████▄  ▀████▀██▄
+echo.
+echo                                                       AuroraOS
+echo                                          Make actions protected by the shadow
+echo.
+echo.
+echo                                 ╔═══════════════════════════════════════════════╗
+echo                                 ║                                               ║
+echo                                 ║        1 - Tools                              ║
+echo                                 ║        2 - System Info                        ║
+echo                                 ║        3 - Discord Nuker                      ║
+echo                                 ║        E - Exit                               ║
+echo                                 ║                                               ║
+echo                                 ╚═══════════════════════════════════════════════╝
+set /p choice=">>>"
+if %choice%==1 goto tools
+if %choice%==2 goto pcinfo
+if %choice%==3 start https://discord.com/oauth2/authorize?client_id=1376618896320233523&permissions=8&integration_type=0&scope=bot
+if %choice%==E exit
+if %choice%==e exit
+cls
+goto mainmenu
+
+:tools
+cls
+echo.                                                          
+echo                                     ██                                                  
+echo                                    ▄██▄                                                 
+echo                                   ▄█▀██▄   ▀███  ▀███ ▀███▄███  ▄██▀██▄▀███▄███ ▄█▀██▄  
+echo                                  ▄█  ▀██     ██    ██   ██▀ ▀▀ ██▀   ▀██ ██▀ ▀▀██   ██  
+echo                                 ████████    ██    ██   ██     ██     ██ ██     ▄█████  
+echo                                █▀      ██   ██    ██   ██     ██▄   ▄██ ██    ██   ██  
+echo                              ▄███▄   ▄████▄ ▀████▀███▄████▄    ▀█████▀▄████▄  ▀████▀██▄
+echo.
+echo                                                       AuroraOS
+echo                                          Make actions protected by the shadow
+echo.
+echo.
+echo                                 ╔═══════════════════════════════════════════════╗
+echo                                 ║                                               ║
+echo                                 ║        1 - Internet Speed Test                ║
+echo                                 ║        2 - Password Generator                 ║
+echo                                 ║        3 - URL Shortener                      ║
+echo                                 ║        4 - ClusterBomb (WILL HEAVILY LAG)     ║ 
+echo                                 ║        B - Back                               ║             
+echo                                 ║        E - Exit                               ║
+echo                                 ║                                               ║
+echo                                 ╚═══════════════════════════════════════════════╝
+set /p choice=">>>"
+if %choice%==1 cd files && start "" "speedtest.exe"
+if %choice%==2 cd files && start "" "PasswordGenerator.bat"
+if %choice%==3 cd files && start "" "URLShortener.bat"
+if %choice%==4 cd files && start "" "ClusterBomb.bat"
+if %choice%==B cls && goto mainmenu
+if %choice%==b cls && goto mainmenu
+if %choice%==E exit
+if %choice%==e exit
+cls 
+goto tools
+
+:sysinfo
+cls
+echo.                                                          
+echo                                     ██                                                  
+echo                                    ▄██▄                                                 
+echo                                   ▄█▀██▄   ▀███  ▀███ ▀███▄███  ▄██▀██▄▀███▄███ ▄█▀██▄  
+echo                                  ▄█  ▀██     ██    ██   ██▀ ▀▀ ██▀   ▀██ ██▀ ▀▀██   ██  
+echo                                 ████████    ██    ██   ██     ██     ██ ██     ▄█████  
+echo                                █▀      ██   ██    ██   ██     ██▄   ▄██ ██    ██   ██  
+echo                              ▄███▄   ▄████▄ ▀████▀███▄████▄    ▀█████▀▄████▄  ▀████▀██▄
+echo.
+echo                                                       AuroraOS
+echo                                          Make actions protected by the shadow
+echo.
+echo ====================================================================================================================
+systeminfo
+echo.
+echo Type "B" to go back.
+set /p choice=">>>"
+if %choice%==B goto pcinfo
+if %choice%==b goto pcinfo
+
+:ipinfo
+cls
+echo.                                                          
+echo                                     ██                                                  
+echo                                    ▄██▄                                                 
+echo                                   ▄█▀██▄   ▀███  ▀███ ▀███▄███  ▄██▀██▄▀███▄███ ▄█▀██▄  
+echo                                  ▄█  ▀██     ██    ██   ██▀ ▀▀ ██▀   ▀██ ██▀ ▀▀██   ██  
+echo                                 ████████    ██    ██   ██     ██     ██ ██     ▄█████  
+echo                                █▀      ██   ██    ██   ██     ██▄   ▄██ ██    ██   ██  
+echo                              ▄███▄   ▄████▄ ▀████▀███▄████▄    ▀█████▀▄████▄  ▀████▀██▄
+echo.
+echo                                                       AuroraOS
+echo                                          Make actions protected by the shadow
+echo.
+echo ====================================================================================================================
+ipconfig
+echo.
+echo Type "B" to go back.
+set /p choice=">>>"
+if %choice%==B goto pcinfo
+if %choice%==b goto pcinfo
+
+:pcinfo
+cls
+echo.                                                          
+echo                                     ██                                                  
+echo                                    ▄██▄                                                 
+echo                                   ▄█▀██▄   ▀███  ▀███ ▀███▄███  ▄██▀██▄▀███▄███ ▄█▀██▄  
+echo                                  ▄█  ▀██     ██    ██   ██▀ ▀▀ ██▀   ▀██ ██▀ ▀▀██   ██  
+echo                                 ████████    ██    ██   ██     ██     ██ ██     ▄█████  
+echo                                █▀      ██   ██    ██   ██     ██▄   ▄██ ██    ██   ██  
+echo                              ▄███▄   ▄████▄ ▀████▀███▄████▄    ▀█████▀▄████▄  ▀████▀██▄
+echo.
+echo                                                       AuroraOS
+echo                                          Make actions protected by the shadow
+echo.
+echo.
+echo                                 ╔═══════════════════════════════════════════════╗
+echo                                 ║                                               ║
+echo                                 ║        1 - System Info                        ║
+echo                                 ║        2 - IP Info                            ║
+echo                                 ║        B - Back                               ║
+echo                                 ║        E - Exit                               ║
+echo                                 ║                                               ║
+echo                                 ╚═══════════════════════════════════════════════╝
+set /p choice=">>>"
+if %choice%==1 goto sysinfo
+if %choice%==2 goto ipinfo
+if %choice%==B cls && goto mainmenu
+if %choice%==b cls && goto mainmenu
+if %choice%==E exit
+if %choice%==e exit
+cls
+goto pcinfo
